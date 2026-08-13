9dd785464a42f6d107faf0a5615218ac02e717d2
Adrean-Dev <adreancassola@gmail.com>
2026-08-13 18:14:55 -0500

    First packing for first version

--- Logger/logger.h
+++ Logger/logger.h
@@ -1,11 +1,20 @@
 #pragma once
 #include <iostream>
+#include <chrono>
 
 
 #define Log(level, sys, msg) std::cout << level << "{" << sys << "} " << msg << std::endl
-
 
 #define INFO "[INFO]: " //Used to provide general information.
 #define WARNING "[WARNING]: " //Used to provide information of something that could create glitches.
 #define ERROR "[ERROR]: " //Used to provide information of something that could create bugs.
 #define FATAL "[FATAL]: " //Used to provide information of something that could/will crash the program.
+
+
+#define MICRO_SECONDS std::chrono::microseconds
+#define MILLI_SECONDS std::chrono::milliseconds
+#define SECONDS std::chrono::seconds
+
+#define TimeShot std::chrono::steady_clock::now
+
+#define elapsed_time(start, end, timeClip) std::chrono::duration_cast<timeClip>(end-start).count()
---
+++ MECA/Example2.cpp
@@ -0,0 +1,52 @@
+#include <meca.h>
+#include <logger.h>
+
+
+struct Health {
+    int value = 100;
+    meca::entityID id;
+};
+
+struct Position {
+    int x = 0;
+    int y = 0;
+    meca::entityID id;
+};
+
+
+int main() {
+    auto start_creating = TimeShot();
+
+    meca::componentRegistry<Health> healths;
+    meca::componentRegistry<Position> positions;
+
+    for(int i = 0; i < 10000; i++) {
+        meca::create_entity();
+        meca::create_component(i, {}, healths);
+        meca::create_component(i, {}, positions);
+    }
+
+    auto end_creating = TimeShot();
+
+    Log(INFO, "MAIN", elapsed_time(start_creating, end_creating, MICRO_SECONDS)); //Between 5000-7500 microseconds
+
+
+    auto start_system = TimeShot();
+
+    for(Health &hp : meca::component_iterator(healths)) {
+        hp.value += 50;
+    }
+
+    meca::filter_for(
+        meca::AND,
+        [](Health &hp, Position &pos) {
+            pos.x += hp.value;
+            pos.y = pos.x*2;
+        },
+        healths, positions
+    );
+
+    auto end_system = TimeShot();
+
+    Log(INFO, "MAIN", elapsed_time(start_system, end_system, MICRO_SECONDS)); //Between 400-800 microseconds
+}
